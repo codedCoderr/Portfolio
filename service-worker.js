73faf3b6ca6189ec51f02fdf1689526c5e5ba455
codedcoderr <codedcoderrr@gmail.com>
2019-12-03 15:30:49 +0100

    ...

--- service-worker.js
+++ service-worker.js
@@ -27,13 +27,13 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
+  // // Perform install steps
+  // event.waitUntil(
+  //   caches.open(CACHE_NAME).then(function(cache) {
+  //     console.log('Opened cache');
+  //     return cache.addAll(urlsToCache);
+  //   })
+  // );
 });
 self.addEventListener('activate', function(event) {
   var cacheWhitelist = [
