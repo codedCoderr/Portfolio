18c17445a6e5da53cd676f08d9650003b226cb39
codedcoderr <codedcoderrr@gmail.com>
2019-12-03 13:26:35 +0100

    ...

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
+self.addEventListener('install', event => {
+  console.log('Inside the install handler:', event);
+});
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
+self.addEventListener('activate', event => {
+  console.log('Inside the activate handler:', event);
+});
+
+self.addEventListener(fetch, event => {
+  console.log('Inside the fetch handler:', event);
 });
